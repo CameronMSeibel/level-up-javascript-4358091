d18e377bdf0554d7daf2fbec2df69759ab72ba6b
Cameron Seibel <cmseibel@uw.edu>
2023-09-26 20:18:31 +0000

    complete exercise 1

--- start/01_01_available_books.js
+++ start/01_01_available_books.js
@@ -1,1 +1,35 @@
-// Write your code here
+/**
+ * Book availability exercise
+ * Class tracks title, author, isbn, numCopies
+ */
+
+class Book {
+
+  constructor(title, author, isbn, numCopies) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+    this.numCopies = numCopies;
+  }
+
+  getAvailability() {
+    if(this.numCopies === 0) {
+      return "out of stock";
+    }
+    if(this.numCopies < 10) {
+      return "low stock";
+    }
+    return "in stock";
+  }
+
+  restock(numCopies = 5) {
+    if(numCopies < 0) return;
+    this.numCopies += numCopies;
+  }
+
+  sell(numSold = 1) {
+    if(numSold < 0) return;
+    this.numCopies -= numSold;
+    if(this.numCopies < 0) this.numCopies = 0;
+  }
+}
