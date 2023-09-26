27650f877eb0f277693324c89a311da602519820
Cameron Seibel <cmseibel@uw.edu>
2023-09-26 21:30:19 +0000

    complete exercise 9

--- start/01_09_coffee_calculations.js
+++ start/01_09_coffee_calculations.js
@@ -1,1 +1,11 @@
 // Write your code here
+
+function calculateBill(orders) {
+  let total = orders.reduce((sum, current) => {
+    return sum + current * 1.25;
+  }, 0);
+
+  return `The total is $${total}`;
+}
+
+console.log(calculateBill([2, 3, 1, 5]));
