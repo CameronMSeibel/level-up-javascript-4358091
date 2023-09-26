6a5862529a6a73ce8178bde3c97c590b7db9decb
Cameron Seibel <cmseibel@uw.edu>
2023-09-26 21:06:42 +0000

    complete exercise 7

--- start/01_07_classroom_attendance.js
+++ start/01_07_classroom_attendance.js
@@ -1,1 +1,17 @@
 // Write your code here
+function getStudents({hasTeachingAssistant, classList}) {
+  let teacher, asst, students;
+  if(hasTeachingAssistant) {
+    [teacher, asst, ...students] = classList;
+  } else {
+    [teacher, ...students] = classList;
+  }
+  return students;
+}
+
+let classroom = {
+  hasTeachingAssistant: false,
+  classList: ["Bob", "Rob", "Dob", "Sob", "Lob"]
+};
+
+console.log(getStudents(classroom));
