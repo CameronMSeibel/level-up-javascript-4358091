775cf648a42616fc401af99451895b948f2bf9f8
Cameron Seibel <cmseibel@uw.edu>
2023-09-26 20:49:18 +0000

    complete exercise 5

--- start/01_05_private_object.js
+++ start/01_05_private_object.js
@@ -1,1 +1,27 @@
 // Write your code here
+
+class User {
+  #username;
+  #password;
+  constructor(username, password, age) {
+    this.#username = username;
+    this.#password = password;
+    this.age = age;
+  }
+}
+
+const user = new User("cmseibel", "12345", 27);
+console.log(user.username);
+console.log(user.password);
+
+// Oh, per the video:
+const username = Symbol("username");
+const password = Symbol("password");
+
+const user2 = {
+  [username]: "cmseibel",
+  [password]: "password123",
+  age: 27
+};
+console.log(user2.username);
+console.log(user2.password);
