005a5937d512002e851a4fcc40c2ea58488a18be
Cameron Seibel <cmseibel@uw.edu>
2023-10-11 18:00:03 +0000

    Complete exercise 14

--- start/01_14_urlify.js
+++ start/01_14_urlify.js
@@ -1,1 +1,9 @@
 // Write your code here
+function urlify(string) {
+  let result = string.toLowerCase();
+  result = result.replace(/[^\w\s]/g, "");
+  result = result.replace(/\s/g, "-");
+  return result;
+}
+
+console.log(urlify("Emma's blog name!"));
