89d701b6cfe89c3c2873de4bb284d9ad36033329
Cameron Seibel <cmseibel@uw.edu>
2023-10-11 18:37:36 +0000

    Complete exercise 15

--- start/01_15_password_strength.js
+++ start/01_15_password_strength.js
@@ -1,1 +1,18 @@
 // Write your code here
+function validatePassword(password) {
+  let lowerCase = /[a-z]/;
+  let upperCase = /[A-Z]/;
+  let oneDigit = /[0-9]/;
+  let oneSpecial = /[^A-Za-z\d\s]/;
+  let eightCharacters = /.{8,}/
+  let isValid = 
+    lowerCase.test(password) &&
+    upperCase.test(password) &&
+    oneDigit.test(password) &&
+    oneSpecial.test(password) &&
+    eightCharacters.test(password);
+  return isValid ? "Yea" : "Nah";
+}
+
+console.log(validatePassword("Password123"));
+console.log(validatePassword("9Ab!4567"));
