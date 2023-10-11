ec6a2afdd0a72ffb79367d0466013d2a4a9d1ca3
Cameron Seibel <cmseibel@uw.edu>
2023-10-11 17:04:29 +0000

    Complete exercise 12

--- start/01_12_string_permutation.js
+++ start/01_12_string_permutation.js
@@ -1,1 +1,14 @@
 // Write your code here
+function nCombinations(string) {
+  function factorial(value) {
+    if(value <= 1) {
+      return 1;
+    } else {
+      return value * factorial(value - 1);
+    }
+  }
+
+  return factorial(string.length);
+}
+
+console.log(nCombinations(""));
